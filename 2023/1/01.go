4e2264389d3fcc31626ba42973e60c7e866bda97
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2023-12-15 23:54:36 -0500

    changes

--- 2023/1/01.go
+++ 2023/1/01.go
@@ -14,11 +14,14 @@ func process() int {
 		2. Read file line by line
 			a. store line as string
 			b. Iterate over string and identify 1st and last digit in string
+				- Update: need to maintain a slice of words and run string comparison with words
+					a) create slice of strings that store each word []string
+					b)
 			c. Combine 1st and last digit to create a number
 		3. Add all numbers (1 per line of input in file) and return as solution
 	*/
 
-	// Use os.open() function to open the file.
+	// Open file
 	// filePtr, err := os.Open("./test.txt")
 	filePtr, err := os.Open("./input.txt")
 	if err != nil {
@@ -26,16 +29,14 @@ func process() int {
 	}
 	defer filePtr.Close()
 
-	// Use bufio.NewScanner() function to create the file scanner.
+	// Create file scanner
 	scanner := bufio.NewScanner(filePtr)
-	// Use bufio.ScanLines() function with the scanner to split the file into lines => DEFAULT behaviour, not required
+	// Split file into lins using newline as deliminator
 	// scanner.Split(bufio.ScanLines)
-	// Then use the scanner Scan() function in a for loop to get each line and process it.
+	// Process each line
 	sum := 0
 	for scanner.Scan() {
-		// fmt.Println(scanner.Text())
 		line := scanner.Text()
-		// fmt.Println(reflect.TypeOf(line))
 		var firstDigit, lastDigit int
 		isFirst := true
 		for _, char := range line {
@@ -47,20 +48,9 @@ func process() int {
 				lastDigit, err = strconv.Atoi(string(char))
 			}
 		}
-		// fmt.Println(line)
-		// fmt.Printf("First digit: %d \nLast digit: %d\n", firstDigit, lastDigit)
 		sum += firstDigit*10 + lastDigit
 		firstDigit, lastDigit = 0, 0
 		isFirst = true
-
-		// for i := 0; i < len(line); i++ {
-		// 	fmt.Println(reflect.TypeOf(line[i]))
-		// 	break
-		// 	fmt.Println(line[i])
-		// }
-		// Iterate over string and identify first and last digit
-		// Combine to create a number
-		// Add to sum
 	}
 	return sum
 }
