f279b36942accbabd4de3aa8b1b3dee3c5e11c88
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2024-02-07 09:20:19 -0500

    Incorrect solution - too low

--- 2023/1/01.go
+++ 2023/1/01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -21,37 +22,66 @@ func process() int {
 		3. Add all numbers (1 per line of input in file) and return as solution
 	*/
 
-	// Open file
-	// filePtr, err := os.Open("./test.txt")
-	filePtr, err := os.Open("./input.txt")
+	// Open file -> returns a file pointer with R access
+	filePtr, err := os.Open("./test.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer filePtr.Close()
 
-	// Create file scanner
+	// Create file scanner  -> takes a os.Reader file pointer, defaults to /n delimiter
 	scanner := bufio.NewScanner(filePtr)
 	// Split file into lins using newline as deliminator
 	// scanner.Split(bufio.ScanLines)
+
 	// Process each line
 	sum := 0
+	numStr := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 	for scanner.Scan() {
+		var digit int
+		digits := []int{}
+		subStr := []rune{}
 		line := scanner.Text()
-		var firstDigit, lastDigit int
-		isFirst := true
+		// fmt.Printf("%s", "---------------------")
+		// fmt.Printf("%v", reflect.TypeOf(line))
+		// fmt.Printf("%s", "---------------------")
+		// fmt.Print(line)
+		fmt.Println("---------------------")
+		fmt.Println(line)
 		for _, char := range line {
-			if unicode.IsDigit(char) && isFirst {
-				isFirst = false
-				firstDigit, err = strconv.Atoi(string(char))
+			// fmt.Printf("%v", reflect.TypeOf(line[i]))
+			// fmt.Println(string(line[i]))
+			// Check for digit!
+			if unicode.IsDigit(char) {
+				if len(subStr) != 0 {
+					// Trigger a comparison and store any digits you find here
+					for i := 0; i < len(numStr); i++ {
+						if strings.Contains(string(subStr), numStr[i]) {
+							digits = append(digits, i)
+							break
+						}
+					}
+					// Clear subStr
+					fmt.Println(string(subStr))
+					subStr = nil
+				}
+				digit, err = strconv.Atoi(string(char))
+				if err == nil {
+					digits = append(digits, digit)
+				}
+				continue
 			}
-			if unicode.IsDigit(char) && !isFirst {
-				lastDigit, err = strconv.Atoi(string(char))
-			}
+			// Not a digit, begin to store substring
+			subStr = append(subStr, char)
 		}
-		sum += firstDigit*10 + lastDigit
-		firstDigit, lastDigit = 0, 0
-		isFirst = true
+		// sum += firstDigit*10 + lastDigit
+		fmt.Println(string(subStr))
+		// firstDigit, lastDigit = 0, 0
+		// isFirst = true
+		sum = sum + (digits[0]*10 + digits[len(digits)-1])
+
 	}
+	// fmt.Printf("digits: %v", digits)
 	return sum
 }
 
