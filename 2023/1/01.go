5cd57ef366d42f2a2bc2807dccbca27447178194
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2024-03-25 09:05:17 -0400

    cleanup comments

--- 2023/1/01.go
+++ 2023/1/01.go
@@ -10,18 +10,6 @@ import (
 )
 
 func process() int {
-	/*
-		1. Open file
-		2. Read file line by line
-			a. store line as string
-			b. Iterate over string and identify 1st and last digit in string
-				- Update: need to maintain a slice of words and run string comparison with words
-					a) create slice of strings that store each word []string
-					b)
-			c. Combine 1st and last digit to create a number
-		3. Add all numbers (1 per line of input in file) and return as solution
-	*/
-
 	// Open file -> returns a file pointer with R access
 	filePtr, err := os.Open("./test.txt")
 	if err != nil {
@@ -46,31 +34,11 @@ func process() int {
 		if len(line) == 0 {
 			continue
 		}
-		// fmt.Printf("%s", "---------------------")
-		// fmt.Printf("%v", reflect.TypeOf(line))
-		// fmt.Printf("%s", "---------------------")
-		// fmt.Print(line)
 		fmt.Println("---------------------")
 		fmt.Println(line)
 		for _, char := range line {
-			// fmt.Printf("%v", reflect.TypeOf(line[i]))
-			// fmt.Println(string(line[i]))
 			// Check for digit!
-			// TODO: Flow of logic needs changing
-			/*
-				if you encounter digit check substring (add if found) and clear it then append the digit
-				else add char to substring and check if substring length > 3 trigger a comparison
-				comparison needs to be a function
-					loop through the substring and find all matches
-					store the first and last index of matches
-					append the digit that has the smallest first index
-					resize the substring to start from last index of first match
-						repeat comparison
-			*/
 			if unicode.IsDigit(char) {
-				// if len(subStr) != 0 {
-				// 	// Trigger a comparison and store any digits you find here
-				// }
 				digit, err = strconv.Atoi(string(char))
 				if err == nil {
 					digits = append(digits, digit)
@@ -88,19 +56,6 @@ func process() int {
 				}
 			}
 		}
-		// Account for remaining substring if line doesn't end in digit
-		// if len(subStr) != 0 {
-		// 	for i := 0; i < len(numStr); i++ {
-		// 		if strings.Contains(string(subStr), numStr[i]) {
-		// 			digits = append(digits, i+1)
-		// 		}
-		// 	}
-		// 	// fmt.Println(string(subStr))
-		// 	subStr = nil
-		// }
-		// sum += firstDigit*10 + lastDigit
-		// firstDigit, lastDigit = 0, 0
-		// isFirst = true
 		fmt.Println(digits)
 		if len(digits) != 0 {
 			sum = sum + (digits[0]*10 + digits[len(digits)-1])
@@ -108,7 +63,6 @@ func process() int {
 		}
 		sum = sum + (digits[0]*10 + digits[0])
 	}
-	// fmt.Printf("digits: %v", digits)
 	return sum
 }
 
