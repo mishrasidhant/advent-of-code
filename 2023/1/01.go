b20bfecebaff1901a717aa7d7427a5c217e31879
mishrasidhant <31853317+mishrasidhant@users.noreply.github.com>
2024-03-31 09:44:19 -0400

    Cleanup

--- 2023/1/01.go
+++ 2023/1/01.go
@@ -29,14 +29,10 @@ func findMatch(subString string) []int {
 	rawMatches := []Comparator{}
 	matches := []int{}
 	numStr := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	// Comparison Logic => Move to function and impliment multiple sorted comparisons
-	// Find each match => NEED TO ENSURE YOU FIND EACH MATCH!!
 	for i := 0; i < len(numStr); i++ {
 		found := strings.Index(string(subString), numStr[i])
 		// Keep slicing and finding till you have all matches
 		if found != -1 {
-			// fmt.Println("Initial String: ", subString)
-			// fmt.Println("FOUND initial string match : ", numStr[i])
 			rawMatches = append(rawMatches, Comparator{digit: i + 1, index: found, length: len(numStr[i])})
 			if (found + len(numStr[i])) >= len(subString) {
 				continue
@@ -44,10 +40,6 @@ func findMatch(subString string) []int {
 			currentString := subString[found+1:]
 			increment := found
 			for nestedFound := strings.Index(string(currentString), numStr[i]); nestedFound != -1; {
-				// fmt.Println("FOUND repeated STRING MATCH: ", i+1)
-				// fmt.Println("Substring: ", currentString)
-				// fmt.Printf("Found: %v, Increment: %v, NestedFound: %v\n", found, increment, nestedFound)
-				// rawMatches = append(rawMatches, Comparator{digit: i + 1, index: (increment + nestedFound), length: len(numStr[i])})
 				rawMatches = append(rawMatches, Comparator{digit: i + 1, index: nestedFound + increment + 1, length: len(numStr[i])})
 				if (nestedFound + len(numStr[i])) >= len(currentString) {
 					break
@@ -55,9 +47,6 @@ func findMatch(subString string) []int {
 				currentString = currentString[nestedFound+1:]
 				increment += nestedFound
 				nestedFound = strings.Index(string(currentString), numStr[i])
-				// fmt.Println("Abount to compare substring:", currentString[found+1:])
-				// found = strings.Index(string(currentString[found+1:]), numStr[i])
-				// fmt.Println("Found:", found)
 			}
 		}
 	}
@@ -68,19 +57,12 @@ func findMatch(subString string) []int {
 	})
 	// Remove overlapping matches
 	for i := 0; i < len(rawMatches)-1; i++ {
-		// fmt.Printf("Raw matches: %#v", rawMatches)
 		m1 := rawMatches[i].index + rawMatches[i].length - 1
-		// fmt.Println("m1: ", m1)
-		// fmt.Println("m2: ", rawMatches[i+1].index)
 		if m1 >= rawMatches[i+1].index {
-			// Delete i+1
-			// fmt.Printf("Raw Matches PRE %+v: ", rawMatches)
 			rawMatches = append(rawMatches[:i], rawMatches[i+1:]...)
-			// fmt.Printf("Raw Matches POST DELETE: %+v", rawMatches)
 		}
 	}
 	for _, match := range rawMatches {
-		// fmt.Println(rawMatches)
 		matches = append(matches, match.digit)
 	}
 	return matches
@@ -116,20 +98,17 @@ func process() int {
 			// Check for digit!
 			if unicode.IsDigit(char) {
 				// Trigger a comparison and reset substring, you've found a number
-				// fmt.Println("About to compare NUMERIC DIGIT : ", string(subStr))
 				match := findMatch(string(subStr))
 				if len(match) > 0 {
 					fmt.Println("STRING: ", match)
 					digits = append(digits, match...)
 				}
-				// fmt.Println("NO MATCH FOUND")
 				subStr = nil
 				// Store the number
 				digit, err = strconv.Atoi(string(char))
 				if err == nil {
 					fmt.Println("DIGIT: ", digit)
 					digits = append(digits, digit)
-					// fmt.Println("Found: ", digit)
 				}
 				continue
 			}
@@ -139,13 +118,11 @@ func process() int {
 		// Account for leftover substring
 		if len(subStr) > 0 {
 			// Trigger a comparison here
-			// fmt.Println("About to compare END OF LINE: ", string(subStr))
 			match := findMatch(string(subStr))
 			if len(match) > 0 {
 				fmt.Println("String: ", match)
 				digits = append(digits, match...)
 			}
-			// fmt.Println("NO MATCH FOUND")
 			subStr = nil
 		}
 		fmt.Println("Digits: :", digits)
@@ -153,9 +130,7 @@ func process() int {
 			fmt.Println("NO DIGITS FOUND!!")
 			continue
 		}
-		// fmt.Println("Digits: ", digits[0]*10, digits[len(digits)-1])
 		sum = sum + (digits[0]*10 + digits[len(digits)-1])
-		// fmt.Println("Sum: ", sum)
 	}
 	return sum
 }
@@ -174,4 +149,11 @@ Found:  eight
 [6 7 2 8]
 Digits:  60 8
 Sum:  866
+*/
+
+/* BUG : twotwo => need first two is getting deleted, fix index off by 1
+twotwosevenonelzlpnmkdqq2rqthff
+STRING:  [2 7 1]
+DIGIT:  2
+Digits: : [2 7 1 2]
 */
